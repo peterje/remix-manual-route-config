edc56edbf0ef2d9e05f4810d50e2d230f2eb73ed
Peter Edmonds <peter@peteredm.com>
2024-05-13 13:48:44 -0700

    fix: ignore all fs routes by default

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,30 @@
-import { vitePlugin as remix } from "@remix-run/dev";
+import { vitePlugin as remix, VitePluginConfig } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import { vercelPreset } from '@vercel/remix/vite';
 import tsconfigPaths from "vite-tsconfig-paths";
 
 installGlobals();
+const webConfig: VitePluginConfig = {
+  presets: [vercelPreset()]
+}
 
+
+const extensionConfig: VitePluginConfig = {
+  ignoredRouteFiles: ["*"],
+  routes(defineRoutes) {
+    return defineRoutes((route) => {
+      route("/", "routes/_index.tsx");
+      route("/edge", "routes/edge.tsx");
+    })
+  },
+}
 export default defineConfig({
   plugins: [
-    remix({
-      routes(defineRoutes) {
-        return defineRoutes((route) => {
-          route("/", "routes/_index.tsx");
-          route("/edge", "routes/edge.tsx");
-        })
-      },
-      presets: [vercelPreset()]
-    }
+    remix(
+      process.env.VITE_BUILD_MODE === "extension" ? extensionConfig : webConfig
     ), tsconfigPaths()],
 });
+
+
+
